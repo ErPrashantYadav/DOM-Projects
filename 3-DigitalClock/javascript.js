2ac94a5cb58a4e55cbfc4be4bad116005e43342a
ErPrashantYadav <prashanty345@gmail.com>
2024-10-05 09:50:34 +0545

    DOM Projects

---
+++ 3-DigitalClock/javascript.js
@@ -0,0 +1,8 @@
+const clock = document.querySelector("#clock")
+
+setInterval( function() {
+    const date = new Date()
+    // const time = date.toLocaleTimeString()
+    clock.innerHTML = date.toLocaleTimeString()
+},1000)
+
